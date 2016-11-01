455d12117aab62be593bac5984ffee06accbc6e7
michaelschratt <mail@mfs-enterprise.com>
2016-11-01 14:08:57 +0100

    Added HTTP POST to module Web

--- malwragent/packages/modules/web.py
+++ malwragent/packages/modules/web.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""the web module provides HTTP communication methods"""
 from __future__ import absolute_import
 
 import requests
@@ -20,44 +21,11 @@ class _FORMAT(object):
 
 
 class Web(AgentModule):
-    """provides basic transportation routines"""
-    # TODO[25/10/2016][bl4ckw0rm] Provide request timeout and ERROR handling
-    """
-    Process Process-1:
-    Traceback (most recent call last):
-      File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/multiprocessing/process.py", line 258, in _bootstrap
-        self.run()
-      File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/multiprocessing/process.py", line 114, in run
-        self._target(*self._args, **self._kwargs)
-      File "/Users/bl4ckw0rm/PycharmProjects/MalwrAgent/malwragent/packages/agent.py", line 30, in __init__
-        self.run()
-      File "/Users/bl4ckw0rm/PycharmProjects/MalwrAgent/malwragent/packages/agent.py", line 193, in run
-        run_client()
-      File "/Users/bl4ckw0rm/PycharmProjects/MalwrAgent/malwragent/packages/agent.py", line 178, in run_client
-        result = run_single_chain(self.chain['CLIENT'])
-      File "/Users/bl4ckw0rm/PycharmProjects/MalwrAgent/malwragent/packages/agent.py", line 156, in run_single_chain
-        output = run_module(module, args=args)
-      File "/Users/bl4ckw0rm/PycharmProjects/MalwrAgent/malwragent/packages/agent.py", line 113, in run_module
-        result = getattr(instance, 'run')()
-      File "/Users/bl4ckw0rm/PycharmProjects/MalwrAgent/malwragent/packages/agentmodule.py", line 29, in run
-        self.output = getattr(self, self.function)(*args)
-      File "/Users/bl4ckw0rm/PycharmProjects/MalwrAgent/malwragent/packages/modules/web.py", line 47, in f_http_get
-        input_ = self.settings['input']
-      File "/Users/bl4ckw0rm/PycharmProjects/MalwrAgent/malwragent/packages/modules/web.py", line 29, in __do_get
-      File "/Library/Python/2.7/site-packages/requests/api.py", line 70, in get
-        return request('get', url, params=params, **kwargs)
-      File "/Library/Python/2.7/site-packages/requests/api.py", line 56, in request
-        return session.request(method=method, url=url, **kwargs)
-      File "/Library/Python/2.7/site-packages/requests/sessions.py", line 475, in request
-        resp = self.send(prep, **send_kwargs)
-      File "/Library/Python/2.7/site-packages/requests/sessions.py", line 596, in send
-        r = adapter.send(request, **kwargs)
-      File "/Library/Python/2.7/site-packages/requests/adapters.py", line 473, in send
-        raise ConnectionError(err, request=request)
-    ConnectionError: ('Connection aborted.', BadStatusLine("''",))
-    """
+    """provide basic transportation routines"""
+
     @staticmethod
     def __do_get(url, data):
+        """process url,data and make HTTP GET request"""
         # TODO[25/10/2016][bl4ckw0rm] do this better
         # TODO[30/10/2016][bl4ckw0rm] validate URI for its domain, path and parameter
         if data is not None:
@@ -73,24 +41,37 @@ class Web(AgentModule):
             else:
                 return True  # req.text
         else:
-            return False  # req.status_code
+            return False  # think about returning the req.status_code
 
     @staticmethod
     def __do_post(url, data):
-        return False  # req.status_code
+        """process url,data and make HTTP POST request"""
+        payload = {
+            'param': data
+        }
+        print payload
+        req = requests.post(url, payload)
+        # print req.status_code
+        if req.status_code == 200:
+            if req.text:
+                return req.text
+            else:
+                return True  # req.text
+        else:
+            return False  # think about returning the req.status_code
 
     @Decorators.args([('url', _FORMAT.URL)])
     @Decorators.config(run_first=True)
     def f_http_get(self, url):
-        """HTTP GET Request"""
+        """send HTTP GET request"""
         input_ = self.settings['input']
         output = self.__do_get(url, input_)
         return output
 
-    @Decorators.args([('url', _FORMAT.URL)])
+    @Decorators.args([('url', _FORMAT.URL), 'parameter'])
     @Decorators.config(run_first=True)
-    def f_http_post(self, url):
-        """HTTP POST Request"""
+    def f_http_post(self, url):  # ask for two args, current only one supported
+        """send HTTP POST request"""
         input_ = self.settings['input']
         output = self.__do_post(url, input_)
         return output
@@ -99,10 +80,10 @@ class Web(AgentModule):
     @Decorators.args([('url', _FORMAT.URL)])
     @Decorators.config(run_first=True)
     def f_retrieve_image(url):
-        """Retrieve an image, Image is stored in memory only
+        """retrieve an image, image is stored in memory only
         :rtype: StringIO object
         :param url: Image URL
         :return: Object of image
         """
-        output = StringIO(requests.get(url).content)
+        output = StringIO(requests.get(url).content)  # Use BytesIO
         return output
